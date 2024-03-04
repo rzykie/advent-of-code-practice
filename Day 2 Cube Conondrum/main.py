38ec51d1ca4f6baa8587c9e4276d96b89fb728c4
Zykie Rallos <zykie1018@gmail.com>
2024-03-04 09:26:15 +0800

    Chore: on hold

--- Day 2 Cube Conondrum/main.py
+++ Day 2 Cube Conondrum/main.py
@@ -26,3 +26,7 @@ In the example above, games 1, 2, and 5 would have been possible if the bag had 
 
 Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
 """
+
+game_id = [1, 2, 3, 4, 5]
+cubes = {"blue": }
+
